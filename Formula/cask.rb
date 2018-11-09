7374b1b9315f10b6ca719c608021a6916c944283
weareoutman <wangshenwei@qq.com>
2018-11-09 17:38:00 +0800

    chore: add cask formula [ci skip]

---
+++ Formula/cask.rb
@@ -0,0 +1,10 @@
+cask 'consoleworkbench' do
+  version '1.0.0'
+  sha512 'cJZl7EZOaXI6NydglZ6QmahtoM/7hhrVFIhAbDhykBZcstSxACY4pBzIj1M95/mIrB2EH/8ucZjCyfC4QBIE9g=='
+
+  url 'https://github.com/easyops-cn/console-workbench/releases/download/v1.0.0/ConsoleWorkbench-1.0.0-mac.zip'
+  name 'Console Workbench'
+  homepage 'https://github.com/easyops-cn/console-workbench'
+
+  app 'ConsoleWorkbench.app'
+end
