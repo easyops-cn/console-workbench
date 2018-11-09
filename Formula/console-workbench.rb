c8d35682603b693dd2c0c015b4287d1128937d31
weareoutman <wangshenwei@qq.com>
2018-11-09 17:49:20 +0800

    fix: use sha256 for cask [ci skip]

--- Formula/console-workbench.rb
+++ Formula/console-workbench.rb
@@ -1,6 +1,6 @@
 cask 'console-workbench' do
   version '1.0.0'
-  sha512 'cJZl7EZOaXI6NydglZ6QmahtoM/7hhrVFIhAbDhykBZcstSxACY4pBzIj1M95/mIrB2EH/8ucZjCyfC4QBIE9g=='
+  sha256 '2ac41696ec7a1d097245463d21aab36e913b4faf6e930fe3c4591e494a0d0ed2'
 
   url 'https://github.com/easyops-cn/console-workbench/releases/download/v1.0.0/ConsoleWorkbench-1.0.0-mac.zip'
   name 'Console Workbench'
