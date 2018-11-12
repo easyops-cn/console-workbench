9652462c2ab185422c2d061b2379fedff2c98409
weareoutman <wangshenwei@qq.com>
2018-11-12 15:38:49 +0800

    chore: cask publish 1.0.1 [ci skip]

--- Formula/console-workbench.rb
+++ Formula/console-workbench.rb
@@ -1,8 +1,8 @@
 cask 'console-workbench' do
-  version '1.0.0'
-  sha256 '2ac41696ec7a1d097245463d21aab36e913b4faf6e930fe3c4591e494a0d0ed2'
+  version '1.0.1'
+  sha256 'e410b1ded25f4e214321ed4a7f0a640742c7a3bd1ba56b7cb0734f3134cc56b6'
 
-  url 'https://github.com/easyops-cn/console-workbench/releases/download/v1.0.0/ConsoleWorkbench-1.0.0-mac.zip'
+  url 'https://github.com/easyops-cn/console-workbench/releases/download/v1.0.1/ConsoleWorkbench-1.0.1-mac.zip'
   name 'Console Workbench'
   homepage 'https://github.com/easyops-cn/console-workbench'
 
