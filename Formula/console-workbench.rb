f7b1cffc9c6640c70c49413a894bb55c3de01c30
Shenwei Wang <wangshenwei@qq.com>
2018-11-20 09:57:01 +0800

    chore: update cask formula to 1.3.2 [ci skip]

--- Formula/console-workbench.rb
+++ Formula/console-workbench.rb
@@ -2,7 +2,7 @@ cask 'console-workbench' do
   version '1.3.2'
   sha256 :no_check
 
-  url 'https://github.com/easyops-cn/console-workbench/releases/download/v1.2.0/ConsoleWorkbench-1.2.0-mac.zip'
+  url 'https://github.com/easyops-cn/console-workbench/releases/download/v1.3.2/ConsoleWorkbench-1.3.2-mac.zip'
   name 'Console Workbench'
   homepage 'https://github.com/easyops-cn/console-workbench'
 
